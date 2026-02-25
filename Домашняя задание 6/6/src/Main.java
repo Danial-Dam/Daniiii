fef16f2c698e77c68d3cf52bbe435ada2005425e
Yumi <dindondin2163@gmail.com>
2026-02-25 15:25:24 +0500

    Add files via upload

--- Домашняя задание 6/6/src/Main.java
+++ Домашняя задание 6/6/src/Main.java
@@ -1,46 +1,90 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        PaymentContext context = new PaymentContext();
 
-        System.out.println("Введите сумму оплаты:");
-        double amount = scanner.nextDouble();
+        System.out.println("===== ВЫБЕРИТЕ ЗАДАЧУ =====");
+        System.out.println("1 - Паттерн Стратегия (Оплата)");
+        System.out.println("2 - Паттерн Наблюдатель (Курсы валют)");
+        System.out.print("Ваш выбор: ");
 
-        System.out.println("Выберите способ оплаты:");
-        System.out.println("1 - Банковская карта");
-        System.out.println("2 - PayPal");
-        System.out.println("3 - Криптовалюта");
-
-        int choice = scanner.nextInt();
+        int taskChoice = scanner.nextInt();
         scanner.nextLine();
 
-        switch (choice) {
+        switch (taskChoice) {
+
+
             case 1:
-                System.out.println("Введите номер карты:");
-                String card = scanner.nextLine();
-                context.setStrategy(new CreditCardPayment(card));
+
+                PaymentContext context = new PaymentContext();
+
+                System.out.print("Введите сумму оплаты: ");
+                double amount = scanner.nextDouble();
+                scanner.nextLine();
+
+                System.out.println("Выберите способ оплаты:");
+                System.out.println("1 - Банковская карта");
+                System.out.println("2 - PayPal");
+                System.out.println("3 - Криптовалюта");
+                System.out.print("Ваш выбор: ");
+
+                int paymentChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (paymentChoice) {
+                    case 1:
+                        System.out.print("Введите номер карты: ");
+                        String card = scanner.nextLine();
+                        context.setStrategy(new CreditCardPayment(card));
+                        break;
+
+                    case 2:
+                        System.out.print("Введите email PayPal: ");
+                        String email = scanner.nextLine();
+                        context.setStrategy(new PayPalPayment(email));
+                        break;
+
+                    case 3:
+                        System.out.print("Введите адрес криптокошелька: ");
+                        String wallet = scanner.nextLine();
+                        context.setStrategy(new CryptoPayment(wallet));
+                        break;
+
+                    default:
+                        System.out.println("Неверный выбор способа оплаты!");
+                        return;
+                }
+
+                context.executePayment(amount);
                 break;
+
 
             case 2:
-                System.out.println("Введите email PayPal:");
-                String email = scanner.nextLine();
-                context.setStrategy(new PayPalPayment(email));
-                break;
 
-            case 3:
-                System.out.println("Введите адрес криптокошелька:");
-                String wallet = scanner.nextLine();
-                context.setStrategy(new CryptoPayment(wallet));
+                CurrencyExchange exchange = new CurrencyExchange();
+
+                IObserver bank = new BankApp();
+                IObserver investor = new InvestorApp();
+                IObserver news = new NewsAgency();
+
+                exchange.addObserver(bank);
+                exchange.addObserver(investor);
+                exchange.addObserver(news);
+
+                System.out.print("Введите валюту: ");
+                String currency = scanner.nextLine();
+
+                System.out.print("Введите курс: ");
+                double rate = scanner.nextDouble();
+
+                exchange.setRate(currency, rate);
                 break;
 
             default:
                 System.out.println("Неверный выбор!");
-                return;
         }
-
-        context.executePayment(amount);
     }
 }
